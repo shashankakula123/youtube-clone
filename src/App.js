052565d85e10f5134b01e121b46b236f3f3be0b0
shashankakula123[A[Agit <shashank310oro@gmail.com>
2022-03-09 15:34:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Header from './Header';
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello there
+           </h1>
+
+            {/* header */}
+
+            <Header/>
+       {/* sidebar */}
+       {/* recommended video */}
+
+    </div>
+
+  );
+}
+
+export default App;
