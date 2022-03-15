4925ea2fa7671e011d179db7959a8d3b4e5788f2
shashankakula123[A[Agit <shashank310oro@gmail.com>
2022-03-15 13:17:08 +0530

    added search

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import AppsIcon from '@mui/icons-material/Apps';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import Avatar from '@mui/material/Avatar';
+import {Link } from 'react-router-dom';
+import './Header.css';
 function Header() {
+
+const [inputSearch,setInputSearch]=useState('');
+
     return (
         <div className='header'>
-<h1>I am a header</h1>
+
+            <div className='header__left'>
+<MenuIcon/>
+<Link to="/">
+<img className="header__logo" src="https://image.shutterstock.com/image-vector/kiev-ukraine-october-20-2021-260nw-2078819083.jpg" alt="youtube" />
+</Link>
+  </div>
+
+<div className='header__input'>
+  <input value={inputSearch} onChange={(e)=>{setInputSearch(e.target.value)}} placeholder='Search' type="text"/>
+ 
+ <Link to={`/search/${inputSearch}`}>
+
+  <SearchIcon className="header__inputbutton" />
+</Link>
+</div>
+
+<div className='header__icons'>
+
+  <VideoCallIcon className='header__icon'/>
+  <AppsIcon className='header__icon'/>
+  <NotificationsOutlinedIcon className='header__icon'/>
+  <Avatar alt="elonmusk" src="https://i.dawn.com/primary/2017/03/58d9f89745239.jpg"/>
+    </div>
         </div>
     )
 }
---
+++ src/RecommendedVideo.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import VideoCard from './VideoCard'
+import './RecommendeVideos.css';
+function RecommendedVideo() {
+    return (
+        <div className='recommendedVideos'>
+            <h2>Recommended</h2>
+
+
+            <div className='recommended__videos'>
+                <VideoCard 
+                title="become a Enterprenuer in 10 minutes"
+                views="2.3M views"
+                timestamp="3 days ago"
+                channelImage="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                channel="Elon Musk"
+                image="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                
+                
+                
+                
+                
+                />
+                <VideoCard 
+                title="become a Enterprenuer in 10 minutes"
+                views="2.3M views"
+                timestamp="3 days ago"
+                channelImage="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                channel="Elon Musk"
+                image="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                
+                
+                
+                
+                
+                />
+                
+
+                <VideoCard 
+                title="become a Enterprenuer in 10 minutes"
+                views="2.3M views"
+                timestamp="3 days ago"
+                channelImage="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                channel="Elon Musk"
+                image="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                
+                
+                
+                
+                
+                />
+                
+
+                <VideoCard 
+                title="become a Enterprenuer in 10 minutes"
+                views="2.3M views"
+                timestamp="3 days ago"
+                channelImage="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                channel="Elon Musk"
+                image="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                
+                
+                
+                
+                
+                />
+                
+
+                <VideoCard 
+                title="become a Enterprenuer in 10 minutes"
+                views="2.3M views"
+                timestamp="3 days ago"
+                channelImage="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                channel="Elon Musk"
+                image="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                
+                
+                
+                
+                
+                />
+                <VideoCard 
+                title="become a Enterprenuer in 10 minutes"
+                views="2.3M views"
+                timestamp="3 days ago"
+                channelImage="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                channel="Elon Musk"
+                image="https://i.pinimg.com/736x/dd/9e/96/dd9e96d34aef0f56dff161f5ac662327.jpg"
+                
+                
+                
+                
+                
+                />
+                
+                
+                </div>
+        </div>
+    )
+}
+
+export default RecommendedVideo
